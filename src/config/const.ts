86b80455685f53c9507e237ca2d016d26034f54f
Ssong-Q <skdmlrh12@gmail.com>
2024-10-21 03:40:09 +0900

    ✨ [Style] globals.css 및 tailwind.config 세팅
    +const.ts 추가

---
+++ src/config/const.ts
@@ -0,0 +1,6 @@
+export const baseDomain = 'https://www.ssongq.dev';
+
+export const blogName = '송규경 개발 블로그';
+export const blogDesc = '기술을 사용한 느낀점과 경험을 공유하고 각종 회고를 적는 블로그입니다.';
+
+export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,85 @@
 import type { Config } from 'tailwindcss';
 
+const createPxEntries = (size: number) => {
+  return {
+    0: '0',
+    ...Array.from(Array(size + 1)).reduce((accumulator, _, i) => {
+      return { ...accumulator, [`${i}`]: `${i / 16}rem` };
+    }),
+  };
+};
+
+const PX_ENTRIES_10 = createPxEntries(10);
+const PX_ENTRIES_100 = createPxEntries(100);
+const PX_ENTRIES_1000 = createPxEntries(1000);
+
 const config: Config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+	prefix: '',
   theme: {
+		borderWidth: PX_ENTRIES_10,
+    fontSize: PX_ENTRIES_100,
+    // spacing values are inherited by the 'padding', 'margin', 'height', 'maxHeight', 'flex-basis', 'gap', 'inset', 'space', 'translate', 'scrollMargin', 'scrollPadding', and 'textIndent'.
+    spacing: PX_ENTRIES_1000,
+		container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+		fontWeight: {
+      light: "200",
+      semiLight: "300",
+      DEFAULT: "400",
+      semibold: "500",
+      bold: "600",
+    },
+		zIndex: {
+      first: "10",
+      second: "20",
+      third: "30",
+      modal: "9990",
+      toast: "9999",
+    },
+		screens: {
+      sm: { max: "768px" },
+      md: { max: "1200px" },
+      // => @media (max-width: 768px) { ... }
+    },
     extend: {
+			fontFamily: {
+        pretendard: ['Pretendard'],
+      },
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
+				transparent: 'transparent',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+        informative: {
+          DEFAULT: 'var(--informative)',
+          foreground: 'var(--informative-foreground)',
+        },
+        warning: {
+          DEFAULT: 'var(--warning)',
+          foreground: 'var(--warning-foreground)',
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
@@ -36,28 +89,84 @@ const config: Config = {
           DEFAULT: 'hsl(var(--accent))',
           foreground: 'hsl(var(--accent-foreground))',
         },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
         },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
+				sm: 'calc(var(--radius) - 4px)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: 'var(--radius)',
+				full: '50%',
+      },
+			keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+				fadeIn: {
+          "0%": { opacity: '0' },
+          "100%": { opacity: '1' },
+        },
+        bounce: {
+          "0%": {
+            transform: "translateY(-12%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+          "100%": {
+            transform: "translateY(-12%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+        },
+        ping: {
+          "50%": {
+            transform: "scale(2)",
+            opacity: '0.7',
+          },
+          "75%": {
+            transform: "scale(2)",
+            opacity: '0.5',
+          },
+          "100%": {
+            transform: "scale(2)",
+            opacity: '0.3',
+          },
+        },
+				textLoop: {
+          '0%': { '-webkit-transform': 'translate3d(0, 0, 0)', transform: 'translate3d(0, 0, 0)' },
+          '100%': { '-webkit-transform': 'translate3d(-100%, 0, 0)', transform: 'translate3d(-100%, 0, 0)' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+				'fadeIn': "fadeIn 2.5s linear",
+        'bounce': "bounce 1.2s linear infinite",
+        'ping': "ping 1s cubic-bezier(0, 0, 0.2, 1) infinite",
+				'textLoop': 'textLoop 10s linear infinite',
+      },
+			backdropBlur: {
+        'custom-blur': '2.5px',
+      },
+			boxShadow: {
+        top: '0 -4px 6px -1px rgba(0, 0, 0, 0.18)',
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
+
 export default config;
