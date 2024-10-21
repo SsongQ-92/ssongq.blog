2446fabbf0bd7574479f212386298735fb679d30
Ssong-Q <skdmlrh12@gmail.com>
2024-10-22 00:36:19 +0900

    ✨ [Feat] Github, LinkedIn Icon 컴포넌트 구현 및 메타 데이터 일부 수정

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,7 @@
 export const baseDomain = 'https://www.ssongq.dev';
 
 export const blogName = '송규경 개발 블로그';
-export const blogDesc = '기술을 사용한 느낀점과 경험을 공유하고 각종 회고를 적는 블로그입니다.';
+export const blogDesc =
+  '기술을 활용한 경험과 느낀 점을 공유하며, 다양한 회고와 인사이트를 기록하는 블로그입니다.';
 
 export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
