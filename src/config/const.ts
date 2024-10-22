2670f7712d54bed69c9e543e17850eb9635d4f83
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-10-22 20:25:06 +0900

    💄 [Chore] not-found page gif 수정

--- src/config/const.ts
+++ src/config/const.ts
@@ -5,7 +5,6 @@ export const blogDesc =
   '기술을 활용한 경험과 느낀 점을 공유하며, 다양한 회고와 인사이트를 기록하는 블로그입니다.';
 
 export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
+export const notFoundGifURL = '/not_found_gif.gif';
 export const githubURL = 'https://github.com/SsongQ-92';
 export const linkedInURL = 'https://www.linkedin.com/in/kyukyoungsong/';
-export const notFoundGifURL =
-  'https://media.giphy.com/media/3ohs7KViF6rA4aan5u/giphy.gif?cid=ecf05e47xhk2cqh66hl2qlmrhfd23c3s0gt2dbkcxl9y6cdq&ep=v1_gifs_search&rid=giphy.gif&ct=g';
