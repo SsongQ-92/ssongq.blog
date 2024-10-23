5d7417e6a9369ae7032af7ae2879c1bafa9f5d67
Ssong-Q <skdmlrh12@gmail.com>
2024-10-23 19:07:54 +0900

    ✨ [Feat] Metadata 및 StaticParams 설정

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,10 +1,39 @@
+import { Metadata } from 'next';
+
 import PostListPage from '../(main)/_components/PostListPage';
+import { baseURL, blogName, blogThumbnailURL } from '@/config/const';
+import { getCategoryList, getCategoryPublicName } from '@/lib/post';
 
 type Props = {
   params: { category: string };
 };
 
 export const dynamicParams = false;
+
+export function generateStaticParams() {
+  const categoryList = getCategoryList();
+  const paramList = categoryList.map((category) => ({ category }));
+  return paramList;
+}
+
+export function generateMetadata({ params: { category } }: Props): Metadata {
+  const cgDisplayName = getCategoryPublicName(category);
+  const title = `${cgDisplayName} | ${blogName}`;
+  const url = `${baseURL}/${category}`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      url,
+      images: [blogThumbnailURL],
+    },
+    twitter: {
+      title,
+      images: [blogThumbnailURL],
+    },
+  };
+}
 
 export default async function CategoryPage({ params }: Props) {
   return <PostListPage category={params.category} />;
