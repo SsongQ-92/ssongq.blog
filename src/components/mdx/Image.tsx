1f7492ba245e2668333b4c131e7bb7bc79a4b1c2
Ssong-Q <skdmlrh12@gmail.com>
2024-10-23 23:19:46 +0900

    ✨ [Feat] Mdx 커스텀 컴포넌트 img 구현

---
+++ src/components/mdx/Image.tsx
@@ -0,0 +1,25 @@
+interface ImageProps {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}
+
+export const Image = ({ src, alt, width, height }: ImageProps) => {
+  return (
+    <>
+      <img
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        className='mx-auto mb-0 mt-35 rounded-md'
+      />
+      {alt !== '' && (
+        <span className='mb-30 mt-8 block w-full text-center text-14 text-gray-500 dark:text-gray-400'>
+          {alt}
+        </span>
+      )}
+    </>
+  );
+};
