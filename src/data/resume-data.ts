8d682c86f48c86d88b425370beac6b724e6e5569
Ssong-Q <skdmlrh12@gmail.com>
2025-03-12 23:58:20 +0900

    ✨ [Feat] 이력서 관련 기본 데이터 상수화

---
+++ src/data/resume-data.ts
@@ -0,0 +1,14 @@
+import { myEmail } from "@/config/const";
+
+export const RESUME_DATA = {
+  name: '송규경',
+  initials: 'SsongQ',
+  location: '대한민국 서울특별시 (한국 표준시)',
+  locationLick: 'https://www.google.com/maps/place/seoul',
+  about: '기본을 중요시하며,  개발자',
+  summary: '불편함을 해결하거나 편리함을 제공하는 프로덕트를 직접 만들 수 있다는 점에서 개발에 큰 매력을 느끼고 있습니다.\n서비스를 더 쉽고 편리하게 이용할 수 있도록 고민하는 과정이 개발의 핵심이라고 생각합니다. 단순히 기능을 구현하는 것을 넘어, 일관된 인터페이스와 예측 가능한 동작을 통해 더 나은 경험을 제공하는 것이 중요하다고 느낍니다. 이러한 이유로 UI/UX에 대한 깊은 관심이 있습니다.\n협업에서는 팀원들 역시 코드의 “사용자”라고 생각하여, 재사용 가능하고 가독성 높은 코드가 작성될 수 있도록 “왜?”라는 질문을 통해 근거 있는 코드를 작성하려 노력합니다. 완성도 높은 기술을 바탕으로, 사용자에게 실질적인 가치를 제공하는 서비스를 개발하는 것이 개발 공부의 동기이자 목표입니다.',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/176848469?v=4',
+  contact: {
+    email: myEmail,
+  },
+}
